2aa4d5be383c84f8d1cf05b7a5ff13ec9e5a80a8
Leo <buxingzhexiong@sina.com>
2019-02-12 11:05:43 +0800

    调整代码结构

--- travel_time_predict.py
+++ travel_time_predict.py
@@ -7,84 +7,107 @@ from keras.layers.recurrent import LSTM
 from keras.layers import Masking
 import os
 
+
+def extract_embeddings(embeddings_file):
+    osmid2embeddings = {}
+    with open(embeddings_file, 'r') as emb_file:
+        for line in emb_file:
+            line = line.strip()
+            osmid_vector = line.split(' ')
+            osmid, node_vec = osmid_vector[0], osmid_vector[1:]
+            if len(node_vec) < 10:
+                continue
+            osmid2embeddings[osmid] = node_vec
+    return osmid2embeddings
+
+
+def extract_samples(samples_file, osmid_embeddings):
+    zero_list = [0 for i in range(128)]
+    with open(samples_file, 'r') as sam_file:
+        line_count = 0
+        for line in sam_file:
+            line_count += 1
+            if line_count % 2000 == 0:
+                print('line_count:', line_count)
+            line = line.strip()
+            nodes_time = line.split(' ')
+            length = len(nodes_time)
+            if 10 > length or length > 1000:
+                continue
+            sample = []
+            bag_line = False
+            for node in nodes_time[:-1]:
+                if node not in osmid_embeddings:
+                    bag_line = True
+                    break
+                node_embeddings = osmid_embeddings[node]
+                tmp_emb = [float(ele) for ele in node_embeddings]
+                sample.append(tmp_emb)
+            target = nodes_time[-1]
+            while len(sample) < 1000:
+                tmp_zero = zero_list.copy()
+                sample.append(tmp_zero)
+            if bag_line:
+                continue
+            else:
+                yield (sample, target)
+
+
+def build_model():
+    # LSTM层的输入必须是三维的
+    # Neural Network model
+    model = Sequential()
+    # model.add(Masking(mask_value=0, input_shape=(1000, 128)))
+    model.add(LSTM(50, input_shape=(1000, 128), return_sequences=False))
+    # model.add(LSTM(100, return_sequences=False))
+    model.add(Dense(1))
+    model.add(Activation('relu'))
+    model.compile(loss="mae", optimizer="adam")
+    model.summary()
+    return model
+
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 emb = 'po_random_1280_128d.emb'
 
 samples_file = 'pt_trajectory_node_travel_time.travel'
 
-osmid_embeddings = {}
+osmid_embeddings = extract_embeddings(emb)
 
-with open(emb, 'r') as emb_file:
-    for line in emb_file:
-        line = line.strip()
-        osmid_vector = line.split(' ')
-        osmid, node_vec = osmid_vector[0], osmid_vector[1:]
-        if len(node_vec) < 10:
-            continue
-        osmid_embeddings[osmid] = node_vec
+samples_targets = extract_samples(samples_file, osmid_embeddings)
 
-r = 4000
+model = build_model()
+
 samples = []
 targets = []
-zero_list = [0 for i in range(128)]
+test_samples = []
+test_targets = []
 
-with open(samples_file, 'r') as sam_file:
-    line_count = 0
-    for line in sam_file:
-        if len(samples) >= r:
-            break
-        line_count += 1
-        if line_count % 1000 == 0:
-            print('line_count:', line_count)
-        line = line.strip()
-        nodes_time = line.split(' ')
-        length = len(nodes_time)
-        if 10 > length or length > 1000:
-            continue
-        sample = []
-        bag_line = False
-        for node in nodes_time[:-1]:
-            if node not in osmid_embeddings:
-                bag_line = True
-                break
-            node_embeddings = osmid_embeddings[node]
-            tmp_emb = [float(ele) for ele in node_embeddings]
-            sample.append(tmp_emb)
-        while len(sample) < 1000:
-            tmp_zero = zero_list.copy()
-            sample.append(tmp_zero)
-        if bag_line:
-            continue
-        else:
-            targets.append(nodes_time[-1])
-            samples.append(sample)
+train_count = 0
+for sample_target in samples_targets:
+    train_count += 1
+    if train_count <= 1100000:
+        (sample, target) = sample_target
+        samples.append(sample)
+        targets.append(target)
+        if len(samples) >= 10000 or train_count == 1100000:
+            assert len(samples) == len(targets)
+            print('training samples at: ', train_count)
+            x_train = np.array(samples)
+            y_train = np.array(targets)
+            BATCH_SIZE = 32
+            epoch = 2
+            model.fit(x_train, y_train, batch_size=BATCH_SIZE, verbose=1, epochs=epoch, validation_split=0.05)
+            samples = []
+            targets = []
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    else:
+        test_samples.append(sample)
+        test_targets.append(target)
+        x_test = np.array(test_samples)
+        y_test = np.array(test_samples)
 
-assert len(samples) == len(targets)
-
-samples = np.array(samples)
-targets = np.array(targets)
-row = round(0.9 * samples.shape[0])
-
-x_train = samples[:int(row)]
-y_train = targets[:int(row)]
-
-x_test = samples[int(row):]
-y_test = targets[int(row):]
-
-# LSTM层的输入必须是三维的
-# Neural Network model
-model = Sequential()
-# model.add(Masking(mask_value=0, input_shape=(1000, 128)))
-model.add(LSTM(100, input_shape=(1000, 128), return_sequences=True))
-model.add(LSTM(100, return_sequences=False))
-model.add(Dense(1))
-model.add(Activation('linear'))
-model.compile(loss="mae", optimizer="rmsprop")
-model.summary()
-BATCH_SIZE = 32
-epoch = 2
-model.fit(x_train, y_train, batch_size=BATCH_SIZE, verbose=1, epochs=epoch, validation_split=0.05)
 metri = model.evaluate(x_test, y_test)
 print(metri)
 
